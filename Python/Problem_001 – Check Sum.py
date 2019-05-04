9cf9279ea9c390cc41823935c3766e107ed52a23
dennismarks <dennis.markovchyn@mail.utoronto.ca>
2019-05-03 21:32:10 -0400

    add #001 python

---
+++ Python/Problem_001 – Check Sum.py
@@ -0,0 +1,15 @@
+def check_pair_sum(array, k):
+    """
+    Given a list of numbers and a number k, return whether any two numbers from
+    the list add up to k.
+
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+    Bonus: Can you do this in one pass?
+    """
+    solutions = set()
+    for number in array:
+        if number in solutions:
+            return True
+        solutions.add(k - number)
+    return False
